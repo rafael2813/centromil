59aacdc8fae20b312fc0f1fceb22446dc6629d80
rafael2813 <ricaichi@gmail.com>
2021-03-05 22:17:40 -0300

    Componente Medida + Geometrias

--- src/components/CalculoPeca.vue
+++ src/components/CalculoPeca.vue
@@ -1,29 +1,23 @@
 <template>
   <v-app>
-    <v-card class="mx-2 my-2" tile elevation=10>
+    <v-card class="mx-2 my-2" tile elevation=5>
       <v-row class="px-4 pt-5 d-flex justify-space-between align-center">
-        <v-col>
-          <v-select :items="perfis" label="Perfil" outlined color="blue darken-4">
-          </v-select>
-        </v-col>
-        <v-col>
-          <v-select :items="secoes" label="Seção Transversal" outlined color="blue darken-4">
-          </v-select>
-        </v-col>
         <v-col>
           <v-select :items="materiais" label="Material" outlined color="blue darken-4">
           </v-select>
         </v-col>
+        <v-col>
+          <v-select v-model="perfil" :items="perfis" @change="perfil_tubo()" label="Perfil" outlined color="blue darken-4">
+          </v-select>
+        </v-col>
+        <v-col>
+          <v-select v-model="secao" :items="secoes" @change="secao_transversal()" label="Seção Transversal" outlined color="blue darken-4">
+          </v-select>
+        </v-col>
       </v-row>
-      <v-row class="px-4 d-flex justify-space-between align-center">
-        <v-col>
-          <Medida />
-        </v-col>
-        <v-col>
-          <Medida />
-        </v-col>
-        <v-col>
-          <Medida />
+      <v-row class="px-4 d-flex justify-space-between align-center" >
+        <v-col v-for="medida in medidas" :key="medida.id" >
+          <Medida :medida="medida" />
         </v-col>
       </v-row>
       <v-row class="px-4 d-flex justify-space-between align-center">
@@ -31,7 +25,7 @@
           <v-text-field v-model="comprimento" type="number" label="Comprimento (metros)" outlined color="blue darken-4"/>
         </v-col>
         <v-col>
-          <v-text-field v-model="valor" type="number" label="Valor por Metro Linear" outlined color="blue darken-4"/>
+          <v-text-field v-model="valor_kg" type="number" label="Preço por Quilo (R$ / kg)" outlined color="blue darken-4"/>
         </v-col>
         <v-col>
           <v-text-field v-model="quantidade" type="number" label="Quantidade de Peças" outlined color="blue darken-4"/>
@@ -39,21 +33,29 @@
       </v-row>
       <v-divider></v-divider>
           <div class="px-4 pt-5 font-weight-black">
-          FÓRMULA DO CÁLCULO DA SEÇÃO TRANSVERSAL = Altura x Largura
+            FÓRMULA DO CÁLCULO DA SEÇÃO TRANSVERSAL = {{ formula }}
           </div>
-          <div class="font-weight-black pb-5">
-            ÁREA SEÇÃO TRANSVERSAL = {{ area }} cm²
+          <div class="px-4 pt-5 font-weight-black">
+            ÁREA SEÇÃO TRANSVERSAL = {{ area | virgula }} cm²
           </div>
-          <div class="font-weight-black pb-5">
-            VOLUME = {{ volume }} cm³
+          <div class="px-4 pt-5 font-weight-black">
+            VOLUME = {{ volume | virgula }} cm³
           </div>
-          <div class="font-weight-black pb-5">
-            PESO ESPECÍFICO = {{ peso_especifico }} kg / m³
+          <div class="px-4 pt-5 font-weight-black">
+            PESO ESPECÍFICO = {{ peso_especifico | virgula }} kg / m³
           </div>
-          <div class="font-weight-black pb-5">
-            PESO POR PEÇA = {{ peso_por_peca }} kg
+          <div class="px-4 pt-5 font-weight-black">
+            PESO POR PEÇA = {{ peso_por_peca | virgula }} kg
           </div>
-          <v-text-field class="font-weight-black" flat readonly solo value="VALOR TOTAL = R$ 125,00"/>
+          <div class="px-4 pt-5 font-weight-black">
+            PREÇO POR PEÇA = {{ preco_por_peca | moeda | virgula }}
+          </div>
+          <div class="px-4 pt-5 font-weight-black">
+            PESO TOTAL = {{ peso_total | virgula }} kg
+          </div>
+          <div class="px-4 py-5 font-weight-black">
+            VALOR TOTAL = {{ valor_total | moeda | virgula }}
+          </div>
     </v-card>
   </v-app>
 </template>
@@ -64,28 +66,94 @@ export default {
   name: 'CalculoPeca',
   components: { Medida },
   data: () => ({
+    polegada: 2.54,
+    pi: 3.14,
     perfis: ['Barra', 'Tubo'],
+    perfil: '',
     secoes: ['Quadrado', 'Redondo', 'Retangular', 'Sextavado'],
+    secao: '',
+    formula: '',
     materiais: ['Alumínio', 'Cobre', 'Latão', 'Ferro'],
-    altura: 0,
-    largura: 0,
-    espessura: 0,
+    medidas: [
+      { id: 0, dimensao: 'Altura', unidade: null, valor: null, visivel: true },
+      { id: 1, dimensao: 'Largura', unidade: null, valor: null, visivel: true },
+      { id: 2, dimensao: 'Espessura', unidade: null, valor: null, visivel: true },
+    ],
     comprimento: null,
-    peso_especifico: 1,
-    valor: null,
+    peso_especifico: null,
+    valor_kg: null,
     quantidade: null,
   }),
   computed: {
     area() {
-      return this.altura * this.largura
+      if (this.secao === 'Quadrado') {
+        return (this.medidas[0].valor ** 2).toFixed(2)
+      }
+      else if (this.secao === 'Redondo') {
+        return ((this.pi * this.medidas[0].valor ** 2) / 4).toFixed(2)
+      }
+      else if (this.secao === 'Retangular') {
+        return (this.medidas[0].valor * this.medidas[1].valor).toFixed(2)
+      }
+      else if (this.secao === 'Sextavado') {
+        return (0).toFixed(2)
+      }
+      return '0.00'
     },
     volume() {
-      return this.area * this.comprimento * 100
+      return (this.area * this.comprimento * 100).toFixed(2)
     },
     peso_por_peca() {
-      return this.volume * this.peso_especifico
+      return (this.volume * this.peso_especifico).toFixed(2)
     },
-  }
+    preco_por_peca() {
+      return (this.peso_por_peca * this.valor_kg).toFixed(2)
+    },
+    peso_total() {
+      return (this.peso_por_peca * this.quantidade).toFixed(2)
+    },
+    valor_total() {
+      return (this.valor_kg * this.peso_total).toFixed(2)
+    },
+  },
+  methods: {
+    perfil_tubo() {
+      this.medidas[2].visivel = this.perfil !== 'Tubo'
+    },
+    secao_transversal() {
+      if (this.secao === 'Quadrado') {
+        this.medidas[0].dimensao = 'Lado'
+        this.medidas[1].visivel = false
+        this.formula = 'Lado x Lado'
+      }
+      else if (this.secao === 'Redondo') {
+        this.medidas[0].dimensao = 'Diâmetro'
+        this.medidas[1].visivel = false
+        this.formula = '3,14 x (Diâmetro x Diâmetro) / 4'
+      }
+      else if (this.secao === 'Retangular') {
+        this.medidas[0].dimensao = 'Altura'
+        this.medidas[1].dimensao = 'Largura'
+        this.medidas[1].visivel = true
+        this.formula = 'Altura x Largura'
+      }
+      else if (this.secao === 'Sextavado') {
+        this.medidas[0].dimensao = 'Altura'
+        this.medidas[1].dimensao = 'Largura'
+        this.medidas[1].visivel = true
+        this.formula = 'Altura x '
+      }
+      
+    },
+  },
+  filters: {
+    moeda: value => {
+      return 'R$ ' + value.toLocaleString('pt-br')
+    },
+    virgula: value => {
+      return value !== null ? value.replace('.', ',') : '0,00'
+    },
+  },
 };
 </script>
 
--- src/components/Medida.vue
+++ src/components/Medida.vue
@@ -1,5 +1,13 @@
 <template>
-  <v-text-field v-model="altura" type="number" label="Altura" outlined color="blue darken-4"/>
+  <v-row  v-show="medida.visivel">
+    <v-col cols="7">
+      <v-text-field v-model="medida.valor" type="number" :label="medida.dimensao" outlined color="blue darken-4"/>
+    </v-col>
+    <v-col cols="5">
+      <v-select :items="unidades" label="Unidade" outlined color="blue darken-4">
+      </v-select>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -10,6 +18,7 @@ export default {
   data: () => ({
     unidades: ['cm', 'mm', 'pol'],
   }),
+  props: ['medida'],
 };
 </script>
 
