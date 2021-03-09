4935957f87ddad4c1a755b94f406b820ad69b759
rafael2813 <ricaichi@gmail.com>
2021-03-08 21:04:39 -0300

    Axios e Fake API

--- src/components/tabela_catalogo/TabelaCatalogo.vue
+++ src/components/tabela_catalogo/TabelaCatalogo.vue
@@ -1,12 +1,75 @@
 <template>
   <v-container px-0 fluid>
-      
+    <v-data-table
+      :page="page"
+      :pageCount="numberOfPages"
+      :headers="headers"
+      :items="passengers"
+      :options.sync="options"
+      :server-items-length="totalPassengers"
+      :loading="loading"
+      class="elevation-1"
+    >
+    </v-data-table>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-
+  name: "DatatableComponent",
+  data() {
+    return {
+      page: 0,
+      totalPassengers: 0,
+      numberOfPages: 0,
+      passengers: [],
+      loading: true,
+      options: {},
+      headers: [
+        { text: "Passenger Name", value: "name" },
+        { text: "Number Of Trips", value: "trips" },
+        { text: "Airline", value: "airline[0].name" },
+        { text: "Logo", value: "logo" },
+        { text: "Website", value: "website" },
+      ],
+    }
+  },
+  //this one will populate new data set when user changes current page. 
+  watch: {
+    options: {
+      handler() {
+        this.readDataFromAPI();
+      },
+    },
+    deep: true,
+  },
+  methods: {
+    //Reading data from API method. 
+    readDataFromAPI() {
+      this.loading = true;
+      const { page, itemsPerPage } = this.options;
+      let pageNumber = page - 1;
+      axios
+        .get(
+          "https://api.instantwebtools.net/v1/passenger?size=" +
+            itemsPerPage +
+            "&page=" +
+            pageNumber
+        )
+        .then((response) => {
+          //Then injecting the result to datatable parameters.
+          this.loading = false;
+          this.passengers = response.data.data;
+          this.totalPassengers = response.data.totalPassengers;
+          this.numberOfPages = response.data.totalPages;
+        })
+    },
+  },
+  //this will trigger in the onReady State
+  mounted() {
+    this.readDataFromAPI();
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import store from './store/store'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  axios,
+  VueAxios,
   render: h => h(App),
   store,
 }).$mount('#app')
